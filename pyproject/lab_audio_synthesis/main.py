dc26a495fff0ab1fa10b8a5cde59baff3dd2af02
kkaral7 <wordpass12>
2017-07-14 16:28:52 -0500

    Merge branch 'master' of https://github.com/mathcircle/ccircle into kara
    # Conflicts:
    #	pyproject/scenario04/readme.txt

--- pyproject/lab_audio_synthesis/main.py
+++ pyproject/lab_audio_synthesis/main.py
@@ -6,6 +6,8 @@ from math import *
 
 window = ccircle.Window('Lab: Audio Synthesis')
 mysound = ccircle.Sound()
+newsound = ccircle.Sound()
+a = ccircle.Sound()
 
 def sawtooth(t, f):
     return 2.0 * ((t * f) % 1.0) - 1.0
@@ -15,6 +17,36 @@ def sine(t, f):
 
 for i in range(44100):
     t = i / 44100
-    mysound.addSample(sine(t, 440))
+    mysound.addSample(sine(t,659 )) #E
+    newsound.addSample(sine(t,587)) #   #D
+    a.addSample(sine(t,523 ))  # C
+    newsound.addSample(sine(t,587))  # #D
+    mysound.addSample(sine(t,659))  # E
+    mysound.addSample(sine(t, 659))  # E
+    mysound.addSample(sine(t, 659))  # E
+    newsound.addSample(sine(t, 587))  # #D
+    newsound.addSample(sine(t, 587))  # #D
+    newsound.addSample(sine(t, 587))  # #D
+
+
 mysound.play()
 time.sleep(1)
+newsound.play()
+time.sleep(1)
+a.play()
+time.sleep(1)
+newsound.play()
+time.sleep(1)
+mysound.play()
+time.sleep(1)
+mysound.play()
+time.sleep(1)
+mysound.play()
+time.sleep(1)
+newsound.play()
+time.sleep(1)
+newsound.play()
+time.sleep(1)
+newsound.play()
+time.sleep(1)
+
